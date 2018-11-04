45830e487584587cd5017a359a2e566903d908b0
Alessandra Camargo <alesouza.camargo@gmail.com>
2018-11-03 22:34:48 -0300

    changes

--- app/src/main/java/br/com/camargoalessandra/doacao/Doar1.kt
+++ app/src/main/java/br/com/camargoalessandra/doacao/Doar1.kt
@@ -11,9 +11,15 @@ class Doar1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doar1)
 
-        btnAddDonationNext1.setOnClickListener(){
-            val addDonationNext = Intent(this, Doar2::class.java)
-            startActivity(addDonationNext)
+        if(itemNome != null && itemDescricao != null) {
+            val doacao = ItemDoacao(itemNome.text.toString(), itemDescricao.text.toString())
+
+            btnAddDonationNext1.setOnClickListener() {
+                val addDonationNext = Intent(this, Doar2::class.java)
+                addDonationNext.putExtra("itemNome", doacao.itemNome)
+                addDonationNext.putExtra("itemDescricao", doacao.itemDescricao)
+                startActivity(addDonationNext)
+            }
         }
     }
 }
--- app/src/main/java/br/com/camargoalessandra/doacao/Doar2.kt
+++ app/src/main/java/br/com/camargoalessandra/doacao/Doar2.kt
@@ -2,17 +2,11 @@ package br.com.camargoalessandra.doacao
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Matrix
-import android.media.ExifInterface
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-import android.os.WorkSource
 import android.provider.MediaStore
 import android.support.v4.content.FileProvider
-import android.view.accessibility.AccessibilityEventSource
+import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_doar2.*
 import java.io.File
@@ -74,36 +68,16 @@ class Doar2 : AppCompatActivity() {
         return arquivoFoto
     }
 
-    fun verificaRotacao(bitmap: Bitmap): Bitmap {
-        val ei = ExifInterface(caminhoFoto)
-        val orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED)
-
-        return when (orientation){
-            ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(bitmap, 90F)
-            ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(bitmap, 180F)
-            ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(bitmap, 270F)
-            else -> bitmap
-        }
-    }
-
-    private fun rotateImage(source: Bitmap, angle:Float):Bitmap {
-        val matrix = Matrix()
-        matrix.postRotate(angle)
-        return Bitmap.createBitmap(source, 0, 0, source.width, source.height, matrix, true)
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == REQUEST_CAMERA && resultCode == Activity.RESULT_OK){
 
-            val arquivoFoto = File(caminhoFoto)
-            if(arquivoFoto.exists()){
-                var bitmap = BitmapFactory.decodeFile(arquivoFoto.absolutePath)
-                bitmap = verificaRotacao(bitmap)
-                foto1.setImageBitmap(bitmap)
-            }
-
+            GlideApp.with(this)
+                    .load(caminhoFoto)
+                    .placeholder(R.drawable.ic_photo_camera)
+                    .centerCrop()
+                    .into(foto1)
         }
 
     }

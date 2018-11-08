99692d420633e046120c067add97aa312630e589
DESKTOP-63NOP9H\pvnan <pvgmenegasso@gmail.com>
2018-11-07 23:55:58 -0200

    splash screen (imagem ainda nao carregando)

--- app/src/main/java/br/com/camargoalessandra/doacao/SplashScreen.kt
+++ app/src/main/java/br/com/camargoalessandra/doacao/SplashScreen.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.bumptech.glide.Glide
+
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
@@ -14,15 +15,16 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
 
-        GlideApp.with (this)
-                .load("https://drive.google.com/file/d/1K98NjY1stdaJ1zQTLT268xwiwEG64rSw/view")
-                //.placeholder(R.mipmap.space)
-                .into(imgLogo)
 
-//        Handler().postDelayed({
-//            val main = Intent(this, Main::class.java)
-//            startActivity(main)
-//            finish()
-//        }, 2000)
+        GlideApp.with(this)
+                 .load("https://drive.google.com/file/d/1K98NjY1stdaJ1zQTLT268xwiwEG64rSw/view")
+                 //.placeholder(R.mipmap.space)
+                 .into(imgLogo)
+
+        Handler().postDelayed({
+            val main = Intent(this, Main2Activity::class.java)
+            startActivity(main)
+            finish()
+        }, 2000)
     }
 }
